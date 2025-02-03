7f9ffc30fa5cc24f2dfda641e72df68ef2774055
fune6900 <riku.riku1019@icloud.com>
2025-02-04 07:12:50 +0900

    Lintチェックの修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [ :create ]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -42,7 +42,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # サインアップ時に許可するパラメータを追加
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :username ])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-  registrations: 'users/registrations'
+  registrations: "users/registrations"
   }
   root "homes#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
