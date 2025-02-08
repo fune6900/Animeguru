7997a1a3750d45e7c8c3520f412d818553870930
fune6900 <riku.riku1019@icloud.com>
2025-02-08 16:37:28 +0900

    placesモデルの制約変更

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,6 +3,6 @@ class Place < ApplicationRecord
 
   # カスタムバリデーション
   validates :name, presence: true, uniqueness: true, length: { maximum: 100 }
-  validates :address, presence: true, length: { maximum: 200 }
-  validates :postal_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/, message: "はXXX-XXXXの形式で入力してください" }
+  validates :address, length: { maximum: 200 }, allow_blank: true
+  validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: "はXXX-XXXXの形式で入力してください" }, allow_blank: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_06_201618) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_08_073301) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,8 +24,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_06_201618) do
 
   create_table "places", force: :cascade do |t|
     t.string "name", null: false
-    t.string "address", null: false
-    t.string "postal_code", null: false
+    t.string "address"
+    t.string "postal_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_places_on_name", unique: true
