f7771803a4c56ef7c55dda5901e0da4e4a482184
fune6900 <riku.riku1019@icloud.com>
2025-02-10 16:42:00 +0900

    SeichiMemosController に show アクションを追加

--- app/controllers/seichi_memos_controller.rb
+++ app/controllers/seichi_memos_controller.rb
@@ -1,10 +1,14 @@
 class SeichiMemosController < ApplicationController
   before_action :authenticate_user!, only: [ :new, :create ]
+  before_action :set_seichi_memo, only: [:show]
 
   require_dependency "seichi_memo_form"
 
   def index
     @seichi_memos = SeichiMemo.includes(:anime, :place).order(created_at: :desc)
+  end
+
+  def show
   end
 
   def new
@@ -15,7 +19,7 @@ class SeichiMemosController < ApplicationController
     @seichi_memo_form = SeichiMemoForm.new(seichi_memo_params)
 
     if @seichi_memo_form.save
-      redirect_to seichi_memos_path, notice: "聖地メモを投稿しました！"
+      redirect_to seichi_memo_path(@seichi_memo_form.seichi_memo), notice: "聖地メモを投稿しました！"
     else
       render :new, status: :unprocessable_entity
     end
@@ -25,5 +29,9 @@ class SeichiMemosController < ApplicationController
 
   def seichi_memo_params
     params.require(:seichi_memo_form).permit(:title, :body, :anime_title, :anime_official_site_url, :place_name, :place_address, :place_postal_code).merge(user_id: current_user.id)
+  end
+
+  def set_seichi_memo
+    @seichi_memo = SeichiMemo.includes(:anime, :place).find(params[:id])
   end
 end
