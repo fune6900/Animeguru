2d52f71cebb7dd474ed05c3ed33196a2d57e0109
fune6900 <riku.riku1019@icloud.com>
2025-02-10 13:54:50 +0900

    聖地メモ投稿画面を表示するコントローラーを作成

--- app/controllers/seichi_memos_controller.rb
+++ app/controllers/seichi_memos_controller.rb
@@ -1,5 +1,11 @@
 class SeichiMemosController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
     @seichi_memos = SeichiMemo.includes(:anime, :place).order(created_at: :desc)
+  end
+
+  def new
+    @seichi_memo_form = SeichiMemoForm.new
   end
 end
