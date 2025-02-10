f3a376f77ad5130b85fbc35fef20d5c2b2bf64e4
fune6900 <riku.riku1019@icloud.com>
2025-02-10 13:52:11 +0900

    各モデルのカスタムバリデーションを削除

--- app/models/anime.rb
+++ app/models/anime.rb
@@ -1,7 +1,3 @@
 class Anime < ApplicationRecord
   has_many :seichi_memos, dependent: :destroy
-
-  # カスタムバリデーション
-  validates :title, presence: true, uniqueness: true, length: { maximum: 100 }
-  validates :official_site_url, format: { with: URI::DEFAULT_PARSER.make_regexp, message: "は正しいURL形式で入力してください" }, allow_blank: true
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,3 @@
 class Place < ApplicationRecord
   has_many :seichi_memos, dependent: :destroy
-
-  # カスタムバリデーション
-  validates :name, presence: true, uniqueness: true, length: { maximum: 100 }
-  validates :address, length: { maximum: 200 }, allow_blank: true
-  validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: "はXXX-XXXXの形式で入力してください" }, allow_blank: true
 end
