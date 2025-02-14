ef72794d28018fdc1ba841146965ca2ad313d7f5
fune6900 <riku.riku1019@icloud.com>
2025-02-14 17:25:56 +0900

    モデルにアップローダーをマウント

--- app/models/seichi_memo.rb
+++ app/models/seichi_memo.rb
@@ -2,4 +2,7 @@ class SeichiMemo < ApplicationRecord
   belongs_to :user
   belongs_to :anime
   belongs_to :place
+
+  mount_uploader :seichi_photo, SeichiPhotoUploader
+  mount_uploader :scene_image, SceneImageUploader
 end
