8acf7b597de260a80dc5a23548e01ad1a1132400
fune6900 <riku.riku1019@icloud.com>
2025-02-17 03:22:33 +0900

    フラッシュメッセージにアニメーション追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,11 @@
 module ApplicationHelper
+  # フラッシュメッセージの背景色を変更
   def flash_background_color(type)
     case type.to_sym
-    when :notice then "bg-green-500"
-    when :alert  then "bg-red-500"
-    when :error  then "bg-yellow-500"
+    when :notice then "bg-green-500"  # 成功（緑）
+    when :alert  then "bg-red-500"    # 失敗（赤）
+    when :warning then "bg-yellow-500" # 警告（黄）
+    when :info   then "bg-blue-500"   # 情報（青）
     else "bg-gray-500"
     end
   end
