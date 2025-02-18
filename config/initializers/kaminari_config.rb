9016c92703f403d9c3512c4278b866045e5a6c68
fune6900 <riku.riku1019@icloud.com>
2025-02-19 01:08:40 +0900

    表記のカスタマイズとページ数の表示制限

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Kaminari.configure do |config|
-  config.default_per_page = 18
+  config.default_per_page = 18 # 1ページあたりの表示件数
+  config.window = 2 # 現在のページの前後に表示するページ数
   # config.max_per_page = nil
   # config.window = 4
   # config.outer_window = 0
