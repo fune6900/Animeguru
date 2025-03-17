5c2cf264002ed4cd4e269efaf396829ad03c7069
fune6900 <riku.riku1019@icloud.com>
2025-03-18 04:45:54 +0900

    Stimlusコントローラの変数名変更

--- app/javascript/controllers/seichi_search_controller.js
+++ app/javascript/controllers/seichi_search_controller.js
@@ -1,18 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["title", "address", "postalCode"]
+  static targets = ["seichi", "address", "postalcode"]
 
   search(event) {
     event.preventDefault(); // フォームの自動送信を防ぐ
 
     // ユーザーが入力した聖地名を取得し、APIリクエストを送信
-    fetch(`/seichi_search?query=${encodeURIComponent(this.titleTarget.value)}`)
+    fetch(`/seichi_search?query=${encodeURIComponent(this.seichiTarget.value)}`)
       .then(response => response.json()) // レスポンスをJSONに変換
       .then(data => {
         // 取得したデータをフォームにセット
         this.addressTarget.value = data.address || ""; // 住所をセット（データがない場合は空）
-        this.postalCodeTarget.value = data.postal_code || ""; // 郵便番号をセット（データがない場合は空）
+        this.postalcodeTarget.value = data.postal_code || ""; // 郵便番号をセット（データがない場合は空）
       });
   }
 }
