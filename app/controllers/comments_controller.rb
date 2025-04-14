8ec74bab24bb6b72b91618b4cc64481baa5d8123
fune6900 <riku.riku1019@icloud.com>
2025-04-15 05:03:51 +0900

    コメントボタンの配置変更とコメント件数表示の配置変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,15 @@
 class CommentsController < ApplicationController
   def create
-    @seichi_memo = SeichiMemo.includes(:anime, :place).find(params[:id])
+    @seichi_memo = SeichiMemo.includes(:anime, :place).find(params[:seichi_memo_id])
     @comment = @seichi_memo.comments.build(comment_params)
     @comment.user = current_user # 現在のユーザーをコメントの作成者として設定
 
     if @comment.save
-      redirect_to seichi_memo_path(@seichi_memo), notice: "コメントを投稿しました。"
+      redirect_to seichi_memo_path(@seichi_memo), notice: "コメントを投稿しました"
     else
-      redirect_to seichi_memo_path(@seichi_memo), alert: "コメントを投稿出来ませんでした。"
+      @comments = @seichi_memo.comments.includes(:user)
+      flash.now[:alert] = "コメントを投稿出来ませんでした"
+      render "seichi_memos/show", status: :unprocessable_entity
     end
   end
 
