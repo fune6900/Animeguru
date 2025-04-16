2f510af000fbc3dc55d5cd9388d6fc49536da202
fune6900 <riku.riku1019@icloud.com>
2025-04-17 06:19:56 +0900

    user.rbやseichi_memo.rbを編集してアソシエーション設定を行う

--- app/models/seichi_memo.rb
+++ app/models/seichi_memo.rb
@@ -3,6 +3,7 @@ class SeichiMemo < ApplicationRecord
   belongs_to :anime
   belongs_to :place
   has_many :comments, dependent: :destroy
+  has_many :bookmarks, dependent: :destroy
 
   mount_uploader :seichi_photo, SeichiPhotoUploader
   mount_uploader :scene_image, SceneImageUploader
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :seichi_memos, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :bookmarks, dependent: :destroy
+  has_many :bookmark_seichi_memos, through: :bookmarks, source: :seichi_memo
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
