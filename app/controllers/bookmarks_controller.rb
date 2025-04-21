c2311165d0d273f7f7aff9f744413d575891dd66
fune6900 <riku.riku1019@icloud.com>
2025-04-21 19:06:57 +0900

    BookmarksControllerをTurbo対応に変更し非同期通信を実現

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,14 +1,24 @@
 class BookmarksController < ApplicationController
+  before_action :authenticate_user!
+
   def create
     @seichi_memo = SeichiMemo.find(params[:seichi_memo_id])
     current_user.bookmark(@seichi_memo)
-    redirect_to seichi_memo_path(@seichi_memo), notice: "聖地メモをブックマークしました！"
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to seichi_memo_path(@seichi_memo) } # 🔹 notice削除
+    end
   end
 
   def destroy
     @bookmark = current_user.bookmarks.find(params[:id])
-    seichi_memo = @bookmark.seichi_memo
-    current_user.unbookmark(seichi_memo)
-    redirect_to seichi_memo_path(seichi_memo), notice: "ブックマークを解除しました！", status: :see_other
+    @seichi_memo = @bookmark.seichi_memo
+    current_user.unbookmark(@seichi_memo)
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to seichi_memo_path(@seichi_memo), status: :see_other } # 🔹 notice削除
+    end
   end
 end
