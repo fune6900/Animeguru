6836200d692a288b21ffaec6b8044e7fff5cfc69
fune6900 <riku.riku1019@icloud.com>
2025-04-21 19:35:18 +0900

    LikesControllerをTurbo対応に変更し非同期通信を実現

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,24 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!
+
   def create
     @seichi_memo = SeichiMemo.find(params[:seichi_memo_id])
     current_user.like(@seichi_memo)
-    redirect_to seichi_memo_path(@seichi_memo)
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to seichi_memo_path(@seichi_memo) }
+    end
   end
 
   def destroy
     @like = current_user.likes.find(params[:id])
-    seichi_memo = @like.seichi_memo
-    current_user.unlike(seichi_memo)
-    redirect_to seichi_memo_path(seichi_memo), status: :see_other
+    @seichi_memo = @like.seichi_memo
+    current_user.unlike(@seichi_memo)
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to seichi_memo_path(@seichi_memo), status: :see_other }
+    end
   end
 end
