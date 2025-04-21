12c5b733033a4ccf8184f6e49fb4626c69626240
fune6900 <riku.riku1019@icloud.com>
2025-04-21 17:47:34 +0900

    create.turbo_stream.erb を生成してTurbo Streamレスポンスを追加。フラッシュメッセージを表示するための修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,29 +1,29 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
     @seichi_memo = SeichiMemo.includes(:anime, :place).find(params[:seichi_memo_id])
     @comment = @seichi_memo.comments.build(comment_params)
-    @comment.user = current_user # 現在のユーザーをコメントの作成者として設定
+    @comment.user = current_user
 
     if @comment.save
       respond_to do |format|
-        format.turbo_stream
-        format.html { redirect_to seichi_memo_path(@seichi_memo), notice: "コメントを投稿しました" }
+        format.turbo_stream do 
+          flash.now[:comment_notice] = "コメントを投稿しました。"
+          render :create
+        end
+        format.html { redirect_to seichi_memo_path(@seichi_memo),  flash: { comment_notice: "コメントを投稿しました。" } }
       end
     else
       @comments = @seichi_memo.comments.includes(:user)
       respond_to do |format|
         format.turbo_stream do
-          flash.now[:alert] = "コメントを投稿出来ませんでした"
-          render turbo_stream: turbo_stream.replace(
-            "comment_form",
-            partial: "comments/form",
-            locals: { seichi_memo: @seichi_memo, comment: @comment }
-          )
+          flash.now[:comment_alert] = "コメントを投稿出来ませんでした"
+          render turbo_stream: [
+            turbo_stream.replace("flash", partial: "shared/flash_message_turbo")
+          ]
         end
-        format.html do
-          flash.now[:alert] = "コメントを投稿出来ませんでした"
-          render "seichi_memos/show", status: :unprocessable_entity
-        end
+        format.html { redirect_to seichi_memo_path(@seichi_memo), flash: { comment_alert: "コメントを投稿出来ませんでした" } }
       end
     end
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,8 @@ module ApplicationHelper
   # フラッシュメッセージの背景色を変更
   def flash_background_color(type)
     case type.to_sym
-    when :notice then "bg-green-500"  # 成功（緑）
-    when :alert  then "bg-red-500"    # 失敗（赤）
+    when :notice, :comment_notice then "bg-green-500"  # 成功（緑）
+    when :alert, :comment_alert  then "bg-red-500"    # 失敗（赤）
     when :warning then "bg-yellow-500" # 警告（黄）
     when :info   then "bg-blue-500"   # 情報（青）
     else "bg-gray-500"
