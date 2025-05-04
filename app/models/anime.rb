1873d10fc1640129d6c0167422f9821ca806ba86
fune6900 <riku.riku1019@icloud.com>
2025-05-04 12:18:07 +0900

    lintチェック修正

--- app/models/anime.rb
+++ app/models/anime.rb
@@ -3,12 +3,12 @@ class Anime < ApplicationRecord
 
   mount_uploader :image_url, AnimeImageUploader
 
-  #ransackで許可するカラム
+  # ransackで許可するカラム
   def self.ransackable_attributes(auth_object = nil)
     %w[title]
   end
 
-  #ransackで許可するテーブル
+  # ransackで許可するテーブル
   def self.ransackable_associations(auth_object = nil)
     %w[seichi_memos]
   end
--- app/models/place.rb
+++ app/models/place.rb
@@ -5,12 +5,12 @@ class Place < ApplicationRecord
   geocoded_by :address
   after_validation :geocode
 
-  #ransackで許可するカラム
+  # ransackで許可するカラム
   def self.ransackable_attributes(auth_object = nil)
     %w[name]
   end
 
-  #ransackで許可するテーブル
+  # ransackで許可するテーブル
   def self.ransackable_associations(auth_object = nil)
     %w[seichi_memos]
   end
