77420efa93815ee563872c82497825b7ac7427c7
fune6900 <riku.riku1019@icloud.com>
2025-05-14 17:49:02 +0900

    onfig/environments/development.rb に SMTPの設定してコンテナ再起動を行う

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,14 +34,20 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # メール送信に失敗した場合はエラーを発生させる
+  config.action_mailer.raise_delivery_errors = true
 
   # Disable caching for Action Mailer templates even if Action Controller
   # caching is enabled.
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+
+  # :letter_opener_web を使って、メールを実際に送信せずブラウザで確認する
+  config.action_mailer.delivery_method = :letter_opener_web
+
+  # メール処理を実行する（false にするとメール生成自体をスキップする）
+  config.action_mailer.perform_deliveries = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
