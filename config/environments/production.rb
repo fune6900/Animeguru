a8a89d684de9a8d9edb99e20a7762ce3e9a6107a
fune6900 <riku.riku1019@icloud.com>
2025-05-20 06:31:14 +0900

    独自ドメインをホワイトリストに追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,6 +110,10 @@ Rails.application.configure do
   #   "example.com",     # Allow requests from example.com
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
   # ]
+  # animeguru.onrender.comを許可するホストとして追加
+  config.hosts << "animeguru.onrender.com"
+  config.hosts << "animeguru.jp"
+  config.hosts << "www.animeguru.jp"
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
