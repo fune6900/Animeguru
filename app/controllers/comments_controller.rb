c6d369f604654beeb7e6c53a9838b21fd544ffac
fune6900 <riku.riku1019@icloud.com>
2025-05-27 18:28:58 +0900

    comments_controller.rbにdestroyアクションを定義

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,8 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_seichi_memo, only: [:create, :destroy]
 
   def create
-    @seichi_memo = SeichiMemo.includes(:anime, :place).find(params[:seichi_memo_id])
     @comment = @seichi_memo.comments.build(comment_params)
     @comment.user = current_user
 
@@ -28,7 +28,35 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment = current_user.comments.find(params[:id])
+
+    if @comment.destroy
+      respond_to do |format|
+        format.turbo_stream do
+          flash.now[:comment_notice] = "コメントを削除しました"
+          render :destroy
+        end
+        format.html { redirect_to seichi_memo_path(@seichi_memo), flash: { comment_notice: "コメントを削除しました" } }
+      end
+    else
+      respond_to do |format|
+        format.turbo_stream do
+          flash.now[:comment_alert] = "コメントを削除出来ませんでした"
+          render turbo_stream: [
+            turbo_stream.replace("flash", partial: "shared/flash_message_turbo")
+          ]
+        end
+        format.html { redirect_to seichi_memo_path(@seichi_memo), flash: { comment_alert: "コメントを削除出来ませんでした" } }
+      end
+    end
+  end
+
   private
+
+  def set_seichi_memo
+    @seichi_memo = SeichiMemo.includes(:anime, :place).find(params[:seichi_memo_id])
+  end
 
   def comment_params
     params.require(:comment).permit(:content)
