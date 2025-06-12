94711a05a58fbf8d748021a3a367f301ddc46eb7
fune6900 <riku.riku1019@icloud.com>
2025-06-12 18:24:45 +0900

    maps_controller.rb作成してindexアクションを定義,app/views/maps/index.html.erbにGoogleマップを表示する

---
+++ app/controllers/maps_controller.rb
@@ -0,0 +1,5 @@
+class MapsController < ApplicationController
+  def index
+    @seichi_memos = SeichiMemo.includes(:anime, :place, :user, :genre_tags).where.not(places: { address: '', latitude: nil, longitude: nil })
+  end
+end
