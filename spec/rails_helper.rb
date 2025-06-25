ed23c84177ded914bc855a459ad559d1afe70141
fune6900 <riku.riku1019@icloud.com>
2025-06-25 20:14:08 +0900

    rspecの動作確認

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,8 +9,9 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 # return unless Rails.env.test?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-require 'simplecov'
-SimpleCov.start 'rails'
+
+# require 'simplecov'
+# SimpleCov.start 'rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -76,7 +77,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
-  config.include LoginMacrosAdd commentMore actions
+  # config.include LoginMacrosAdd commentMore actions
 
   # system spec
   config.before(:each, type: :system) do
