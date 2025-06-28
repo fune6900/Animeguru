ef1ba53c79eabdb06125041d2ea691b523073cfc
fune6900 <riku.riku1019@icloud.com>
2025-06-29 07:10:52 +0900

    テストコードの追加

--- spec/models/anime_spec.rb
+++ spec/models/anime_spec.rb
@@ -9,32 +9,32 @@ RSpec.describe Anime, type: :model do
     end
 
     it "空だと無効になる" do
-      anime = build(:anime, title: '')
+      anime = build(:anime, title: "")
       expect(anime).to be_invalid
-      expect(anime.errors[:title]).to include('を入力してください')
+      expect(anime.errors[:title]).to include("を入力してください")
     end
 
     it "101文字以上だと無効になる" do
-      anime = build(:anime, title: 'a' * 101)
+      anime = build(:anime, title: "a" * 101)
       expect(anime).to be_invalid
-      expect(anime.errors[:title]).to include('は100文字以内で入力してください')
+      expect(anime.errors[:title]).to include("は100文字以内で入力してください")
     end
 
     it "重複したタイトルは無効になる" do
       anime1 = create(:anime)
-      anime2 = build(:anime, title: 'title')
+      anime2 = build(:anime, title: "title")
       expect(anime2).to be_invalid
-      expect(anime2.errors[:title]).to include('はすでに存在します')
+      expect(anime2.errors[:title]).to include("はすでに存在します")
     end
 
     it "URL形式でないと無効になる" do
-      anime = build(:anime, official_site_url: 'not_a_url')
+      anime = build(:anime, official_site_url: "not_a_url")
       expect(anime).to be_invalid
-      expect(anime.errors[:official_site_url]).to include('は正しいURL形式で入力してください')
+      expect(anime.errors[:official_site_url]).to include("は正しいURL形式で入力してください")
     end
 
     it "空欄でも有効である" do
-      anime = build(:anime, official_site_url: '')
+      anime = build(:anime, official_site_url: "")
       expect(anime).to be_valid
       expect(anime.errors).to be_empty
     end
