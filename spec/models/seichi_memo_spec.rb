08cc6c2822fd92b11460128dfa219215e5871bde
fune6900 <riku.riku1019@icloud.com>
2025-06-28 16:52:03 +0900

    テストコード修正

--- spec/models/seichi_memo_spec.rb
+++ spec/models/seichi_memo_spec.rb
@@ -3,30 +3,30 @@ require 'rails_helper'
 RSpec.describe SeichiMemo, type: :model do
   describe 'バリデーションチェック' do
     it '設定したすべてのバリデーションが機能しているか' do
-      seichi_memo = FactoryBot.build(:seichi_memo)
+      seichi_memo = build(:seichi_memo)
       expect(seichi_memo).to be_valid
     end
 
     it 'titleが空だと無効になる' do
-      seichi_memo = FactoryBot.build(:seichi_memo, title: '')
+      seichi_memo = build(:seichi_memo, title: '')
       expect(seichi_memo).to be_invalid
       expect(seichi_memo.errors[:title]).to include('を入力してください')
     end
 
     it 'titleが31文字以上だと無効になる' do
-      seichi_memo = FactoryBot.build(:seichi_memo, title: 'あ' * 31)
+      seichi_memo = build(:seichi_memo, title: 'a' * 31)
       expect(seichi_memo).to be_invalid
       expect(seichi_memo.errors[:title]).to include('は30文字以内で入力してください')
     end
 
     it 'bodyが空だと無効になる' do
-      seichi_memo = FactoryBot.build(:seichi_memo, body: '')
+      seichi_memo = build(:seichi_memo, body: '')
       expect(seichi_memo).to be_invalid
       expect(seichi_memo.errors[:body]).to include('を入力してください')
     end
 
     it 'bodyが501文字以上だと無効になる' do
-      seichi_memo = FactoryBot.build(:seichi_memo, body: 'あ' * 501)
+      seichi_memo = build(:seichi_memo, body: 'a' * 501)
       expect(seichi_memo).to be_invalid
       expect(seichi_memo.errors[:body]).to include('は500文字以内で入力してください')
     end
