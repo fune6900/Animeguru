704268dd4b93f95186fb60ecc108904120bf9cb9
fune6900 <riku.riku1019@icloud.com>
2025-07-03 07:12:39 +0900

    テストコードの追加

--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -1,8 +1,8 @@
 module LoginMacros
   def login_as(user)
     visit new_user_session_path
-    fill_in 'メールアドレス', with: user.email
-    fill_in 'パスワード', with: user.password
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
     click_button 'ログイン'
     expect(page).to have_content("ログインしました。さっそく聖地メモを投稿してみませんか？")
   end
