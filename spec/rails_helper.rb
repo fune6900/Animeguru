73a5e36b3e7b0ee9b43f3fd37ce623fad892b8c7
fune6900 <riku.riku1019@icloud.com>
2025-07-03 01:42:20 +0900

    login_macros.rbを作成してlogin_as(user)モジュールを定義する

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -77,7 +77,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
-  # config.include LoginMacrosAdd commentMore actions
+  config.include LoginMacros
 
   # system spec
   config.before(:each, type: :system) do
